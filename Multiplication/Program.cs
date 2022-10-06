4e2e36d5305fdaa528c4c2684651745af8d26803
ThinksSo <stpt.mail@yandex.ru>
2022-10-06 14:31:53 +0300

    Метод для таблицы  умножения до заданного числа

--- Multiplication/Program.cs
+++ Multiplication/Program.cs
@@ -7,13 +7,14 @@ void Task8_1()
 {
     Console.WriteLine("\n \t Task 8: Multiplication table");
     Console.WriteLine();
+
     Random random = new Random();
     int num = random.Next(2, 30);
     int count = 2;
 
     while (count < num + 1)
     {
-        for (int i = 1; i <= num; i++)
+        for (int i = 2; i <= num; i++)
         {
             Console.WriteLine($"{count} * {i} = {i * count}");
         }
@@ -29,12 +30,12 @@ void Task8_2()
     Console.WriteLine("\n \t Task 8: Multiplication table");
     Console.WriteLine();
     Random random = new Random();
-    int num = random.Next(2, 8);
+    int num = random.Next(3, 9);
     int count = 2;
 
     while (count < num + 1)
     {
-        for (int i = 1; i <= num; i++)
+        for (int i = 2; i <= num; i++)
         {
             Console.Write($"{i} * {count} = {i * count} \t");
             // Печать столбиков рядом (актуально для значений до 8)
@@ -45,6 +46,23 @@ void Task8_2()
     Console.WriteLine();
 }
 
+// Метод для умножнеия до заданного числа
+void Multiplication(int number)
+{
+    int count = 2;
+    while (count < number + 1)
+    {
+        for (int i = 2; i <= number; i++)
+        {
+            Console.WriteLine($"{count} * {i} = {i * count}");
+        }
+        Console.WriteLine();
+        count++;
+    }
+}
+
 Task8_1();
 Task8_2();
+Multiplication(7); // любое значени до которого вывести таблицу умножения
+
 
